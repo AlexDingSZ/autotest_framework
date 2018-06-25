e6bc1b7dcd8cef7debeb4da89168552704731d4a
test <test@test.com>
2018-06-25 14:48:16 +0800

    auto run

--- case/run_case.py
+++ case/run_case.py
@@ -16,12 +16,14 @@ from utils.logger import logger
 
 logger = logger(logger="test_login").getlog()
 if __name__ == "__main__":
+    logger.info("start run test")
     current_file_path = os.path.dirname(__file__)
     root_path = os.path.abspath(os.path.dirname(current_file_path) + os.path.sep + ".")
     case_path = os.path.join(root_path, "case")
     report_path = os.path.join(root_path, "report")
     now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
     report_path = os.path.join(report_path,"auto_result_"+now+".html")
+    logger.info(report_path)
     report_file = open(report_path,"wb")
     logger.info("report_path:%s" % report_file)
 
